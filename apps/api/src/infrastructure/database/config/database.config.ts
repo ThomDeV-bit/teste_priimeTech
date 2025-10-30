ac456409b3416b695826280e3136e121fd54b379
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-30 17:06:20 -0400

    fix: change database driver and enviroments

--- apps/api/src/infrastructure/database/config/database.config.ts
+++ apps/api/src/infrastructure/database/config/database.config.ts
@@ -2,15 +2,16 @@ import { DataSourceOptions, DataSource } from "typeorm";
 import "dotenv/config";
 import { ProfileEntity } from "apps/api/src/domain/entities/profile.entity";
 import { UserEntity } from "apps/api/src/domain/entities/user.entity";
+import { AccessLogEntity } from "apps/audit-log/src/database/entities/access-logs.entity";
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_LOCAL_PORT),
-  username: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  entities: [UserEntity, ProfileEntity],
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_LOCAL_PORT),
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: [UserEntity, ProfileEntity, AccessLogEntity],
   synchronize: true,
   migrations: [],
 };
