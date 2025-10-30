a6cb8fd19e326cd273d7e7edf3b42c69941fb7b9
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-30 14:32:35 -0400

    fix: change path

--- src/domain/interfaces/IUserRepository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { UserDto } from "src/api/dtos/user.dto";
-import { UserEntity } from "../entities/user.entity";
-
-export interface IUserRepository {
-    create(user: UserDto): Promise<boolean>
-    findByEmail(email : string) : Promise<UserEntity | null>
-}
