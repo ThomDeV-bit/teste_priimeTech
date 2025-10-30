ed3894a8a78a24bdfd08a8920f9069eb02e0d8b5
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-30 14:40:30 -0400

    feat: audit microservice app

---
+++ apps/audit-log/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AuditLogModule } from './audit-log.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuditLogModule , {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'audit_log',
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+  await app.listen()
+
+  console.log('🚀 Microserviço de logs conectado ao RabbitMQ');
+}
+
+bootstrap();
