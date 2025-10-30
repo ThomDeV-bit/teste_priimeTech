7609eba81adc725d77e569a051eb93bee3644dd2
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-30 14:38:32 -0400

    feat : create use case dynamic module

---
+++ apps/api/src/use-cases/use-case.module.ts
@@ -0,0 +1,31 @@
+import { DynamicModule, Module } from "@nestjs/common";
+import { UserUsecase } from "./user/user.use-case";
+import { AuthUsecase } from "./auth/auth.use-case";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+
+@Module({})
+export class UseCaseModule {
+  static register(): DynamicModule {
+    return {
+      module: UseCaseModule,
+      imports: [
+        ClientsModule.register([
+          {
+            name: "AUDIT_SERVICE",
+            transport: Transport.RMQ,
+            options: {
+              urls: ["amqp://localhost:5672"],
+              queue: "audit_log",
+              queueOptions: {
+                durable: false,
+              },
+            },
+          },
+        ]),
+      ],
+      global: true,
+      providers: [UserUsecase, AuthUsecase],
+      exports: [UserUsecase, AuthUsecase],
+    };
+  }
+}
